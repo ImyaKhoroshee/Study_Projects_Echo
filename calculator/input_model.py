ed5e2cdb52dd17da451fd277d6a8d5beafd0d0cc
tatyana <tilkka@mail.ru>
2022-06-13 17:15:39 +0300

    First

---
+++ calculator/input_model.py
@@ -0,0 +1,12 @@
+
+
+def greeting_and_instruction():
+
+    from prettytable import PrettyTable
+
+    print()
+    print('         Привет, перед Вами калькулятор для работы с рациональными и комплексными числами.')
+    print()
+    print('Краткая инструкция по вводу данных. ')
+
+greeting_and_instruction()
