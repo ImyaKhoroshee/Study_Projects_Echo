08fa4aa8d02762f6eb0832bf8a58d9b182326fed
tatyana <tilkka@mail.ru>
2022-06-15 00:24:28 +0300

    Changes in pretty input modul

--- calculator/pretty_input.py
+++ calculator/pretty_input.py
@@ -1,20 +1,60 @@
-from prettytable import PrettyTable  
+from prettytable import PrettyTable  # pip install PrettyTable
+import colorama         # pip install colorama
+from colorama import Fore, Back, Style
 
-print()
-print('     Перед Вами калькулятор для работы с рациональными и комплексными числами.')
-print()
-print('                     Валидные данные (знаки операций):')
-name = ['Сложение','Вычитание','Умножение','Деление','Целая часть дроби','Дробь']
-data = ['+','-','*',':','_ (пример: 4_)','/ (пример:1/3)']
+def pretty_input():
+    colorama.init()
+    print()
+    print(Fore.BLACK + '                    КАЛЬКУЛЯТОР')
+    print(Fore.BLACK + '  для работы с рациональными и комплексными числами \n')
+    print(Style.BRIGHT, end='')
+    print(Fore.RED + '              Инструкция пользователя\n')
+    print(Fore.BLUE + ' Арифметические действия и простые дроби обозначаются:')
+    print(Style.BRIGHT, end='')
 
-columns = len(name)  
-table = PrettyTable(name)  
-data_data = data[:]     
+    name = ['Сложение','Вычитание','Умножение','Деление','Дробь']
+    data = ['+','-','*',':','/']
 
-while data_data:    
-    table.add_row(data_data[:columns])
-    data_data = data_data[columns:]
+    columns = len(name)  
+    table = PrettyTable(name)  
+    data_data = data[:]     
 
-print(table)
-print()
-string = input('Введите то, что нужно вычислить ОДНОЙ СТРОКОЙ и нажмите клавишу "ввод": ')
+    while data_data:    
+        table.add_row(data_data[:columns])
+        data_data = data_data[columns:]
+    print(Fore.BLUE, end='')
+    print(table)
+    print()
+
+    name1 = ['Пример записи дроби с целой частью','Пример записи мнимой части']
+    data1 = ['4_1/47','2i2i или 10i2:3i']
+
+    columns1 = len(name1)  
+    table1 = PrettyTable(name1)  
+    data1_data1 = data1[:]     
+
+    while data1_data1:    
+        table1.add_row(data1_data1[:columns1])
+        data1_data1 = data1_data1[columns1:]
+    print(Fore.BLUE, end='')
+    print(table1)
+    print(Fore.GREEN)
+
+    string = input('Введите ОДНОЙ СТРОКОЙ выражение, значение которого нужно найти, и нажмите клавишу "ввод":\n')
+
+pretty_input()
+
+# Fore: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
+# Back: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
+# Style: DIM, NORMAL, BRIGHT, RESET_ALL
+
+# colorama.init() # обязательно для Windows 10.
+# print(Fore.BLACK + 'Черный текст яркий')
+# print(Style.BRIGHT)
+# print(Fore.RED + 'Красный текст')
+# print(Fore.GREEN + 'Зеленый цвет')
+# print(Fore.BLUE + 'Синий цвет')
+# print(Fore.MAGENTA + 'Пурпурный цвет')
+# print(Fore.CYAN + 'Цвет морской волны')
+# print(Fore.RESET)
+# print(Style.RESET_ALL) # это сброс цветов консоли к начальным значениям.
