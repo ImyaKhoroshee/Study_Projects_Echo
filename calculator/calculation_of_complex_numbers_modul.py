20990ff83e3f53891c37ee4212b1c41a22eaa015
Serega S <serega1067@yandex.ru>
2022-06-18 18:46:57 +0700

    Добавил модуль деления целых чисел

--- calculator/calculation_of_complex_numbers_modul.py
+++ calculator/calculation_of_complex_numbers_modul.py
@@ -3,13 +3,77 @@
 '''
 
 # f = "-3+i+5-2i^3"
-f = "3*i+1:i+10*i*2:3*i+21/2*i*2*i"
+# f = "3*i+1:i+10*i*2:3*i+21/2*i*2*i"
+# f = "1:1i:1i"
+# f = "8:2i:1i"
+# f = "2:3i"
+# f = "2i:3i"
+f = "2i:3"
 # return "23", ""
 
+def clear_num_i(arg):
+    """
+    Функция разделяет и возвращает по отдельности число и мнимую i
+    """
+    if arg[0] == "i":
+        return 1, "i"
+    elif arg[0] == "-" and arg[1] == "i":
+        return -1, "i"
+    else:
+        return int(arg[:-1]), "i"
+
+def math_num_i(arg: int):
+    if arg > 1:
+        pass
+
+def math_div_int(arg1, arg2):
+    """
+    Функция делит два числа полученных из функции math_div()
+    и возвращает целое число, если числа делятся без остатка или
+    дробь
+    """
+    if arg1 % arg2 == 0:
+        result = str(arg1 // arg2)
+    else:
+        result = str(arg1) + "/" + str(arg2)
+    return result
+
+def math_div(arg_num1, arg_num2):
+    """
+    Функция принимает список из двух чисел в виде строк и отправляет
+    их в функцию деления дробных чисел или целых чисел
+    """
+    if "/" in [arg_num1, arg_num2]:
+        pass
+    else:
+        if "i" in arg_num1 and "i" in arg_num2:
+            num1 = clear_num_i(arg_num1)[0]
+            num2 = clear_num_i(arg_num2)[0]
+            result = math_div_int(num1, num2)
+        elif "i" in arg_num1 and "i" not in arg_num2:
+            num1, num_i = clear_num_i(arg_num1)
+            num2 = int(arg_num2)
+            result = math_div_int(num1, num2) + num_i
+        elif "i" not in arg_num1 and "i" in arg_num2:
+            num1 = int(arg_num1)
+            num2, num_i = clear_num_i(arg_num2)
+            result = "-" + math_div_int(num1, num2) + num_i
+    return result
+
 def math_calc(arg):
+    """
+    Функция перебирает список из мат. формул и отправляет их
+    в разные функции соответствующие нужной мат. операции
+    """
     for c in arg:
         if "*" in c or ":" in c:
-            decomposition_formula(c)
+            stack_num, stack_char = decomposition_formula(c)
+            print(stack_num, stack_char)
+            for i in range(len(stack_char)):
+                if stack_char[i] == ":":
+                    temp_result = math_div(stack_num[i], stack_num[i+1])
+                    stack_num[i+1] = temp_result
+            print(stack_num)
     # return real_number, imaginary_number
 
 def preparation_formula_separation(arg_f):
@@ -46,10 +110,8 @@ def decomposition_formula(arg):
             stack_char.append(arg[i])
         else:
             num += arg[i]
-            print(num)
     stack_num.append(num)
     num = ""
-    print(stack_num, stack_char)
     return stack_num, stack_char
 
 f1 = preparation_formula_separation(f)
