7ed32eedbcbdb5287a476a35eb3b0bcc8d26441f
Serega S <serega1067@yandex.ru>
2022-06-21 00:16:19 +0700

    В модуле calc_logging.py добавил кодировку utf-8, изменил очерёдность написания даты, сменил язык заголовков на русский

--- calculator/calc_logging.py
+++ calculator/calc_logging.py
@@ -7,16 +7,25 @@
 import datetime
 
 def read_data_file(arg_formula, arg_solved_formula):
-    dt_now = str(datetime.datetime.now())
-    data = open("use_logs.txt", "a")
+    dt_now_temp = datetime.datetime.now()
+    dt_now = "{0}:{1}:{2}\nдата:\n{3}.{4}.{5}".format(
+        dt_now_temp.hour,
+        dt_now_temp.minute,
+        dt_now_temp.second,
+        dt_now_temp.day,
+        dt_now_temp.month,
+        dt_now_temp.year
+    )
+    data = open("use_logs.txt", "a", encoding="utf-8")
     data.seek(0)
-    data.write("user formula:\n"
+    data.write("формула пользователя:\n"
                "{0}\n"
-               "solution result:\n"
+               "результат вычислений:\n"
                "{1}\n"
-               "date:\n"
+               "расчёты записаны в:\n"
                "{2}\n-------------------------\n"
-               .format(arg_formula, arg_solved_formula, dt_now))
+               .format(arg_formula, arg_solved_formula, dt_now)
+               )
     data.close()
 
 formula_user = "a+b=c"
