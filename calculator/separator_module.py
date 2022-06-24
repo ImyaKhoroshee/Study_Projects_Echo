0607188e0c50fa50d99f7d5b3c6d5d11d230fb60
FocusNK <49724976+FocusNK@users.noreply.github.com>
2022-06-24 14:27:28 +0700

    separator v.2.0
    new function

--- calculator/separator_module.py
+++ calculator/separator_module.py
@@ -1,65 +1,28 @@
 import re
-def separator (string_conversion):    
-    listRes = []
-    listRes2 = []
+
+def separator(input_string):
+    temp = ''
+    result=[]
+    set_before_minus = {':','*','^',''}
+    clean = []
+    clean2 = []
     clean_equation = []
-    temp_conversion = []
-    token_start = [ '+', '^', '*','/',':','&']
-    token_end = ['-', '+', '^', '*','/',':','&']
-    temp_conversion = string_conversion.replace('^','&')
-    sort = re.findall(r'[-+]\d+[/]\d+\w[:*^&]\d+[/]\d+\w|[-+]\d+[/]\d+\w[:*^&]\d+[/]\d+|[-+]\d+[/]\d+[:*^&]\d+[/]\d+\w|[-+]\d+[/]\d+\w[:*^&]\d+\w|[-+]\d+\w[:*^&]\d+[/]\d+\w|[-+]\d+[/]\d+[:*^&]\d+\w|[-+]\d+[:*^&]\d+[/]\d+\w|[-+]\d+[/]\d+\w[:*^&]\d+|[-+]\d+\w[:*^&]\d+[/]\d+|[-+]\d+\w[:*^&]\d+\w|[-+]\d+[:*^&]\d+\w|[-+]\d+\w[:*^&]\d+|\d+[/]\d+\w[:*^&]\d+[/]\d+\w|\d+[/]\d+\w[:*^&]\d+[/]\d+|\d+[/]\d+[:*^&]\d+[/]\d+\w|\d+[/]\d+[:*^&]\d+\w|\d+[:*^&]\d+[/]\d+\w|\d+[/]\d+\w[:*^&]\d+|\d+\w[:*^&]\d+[/]\d+|\d+\w[:*^&]\d+\w|\d+[:*^&]\d+\w|\d+\w[:*^&]\d+|[-+]\d+[/]\d+\w|\d+[/]\d+\w|\d+\w|\d+\D\w|\D\d+', temp_conversion)
-
-
-    for i in sort:
-        if "i" in i:
-            listRes.append(i)
+    for i,symb in enumerate(input_string[:-1]):
+        
+        if symb == "+" or (symb == "-" and input_string[i-1] not in set_before_minus) :
+            result.append(temp)
+            temp =''
+        temp += symb
+    temp += input_string[-1]
+    result.append(temp)
+    for x in result:
+        if any("i" in filt for filt in x) == True:
+            clean.append(x)
         else:
-            listRes2.append(i)
-
-    listRes = ''.join(listRes)
-
-    for j in listRes:
-        if listRes == []:
-            break
-        else:
-            for i in token_start: # проверка  начала списка и конца, на "мусорные" символы и удаление
-                if listRes[0] == i:
-                    listRes.pop(0)
-            for i in token_end:
-                if listRes[-1] == i:
-                    listRes.pop(-1)
-    listRes = ''.join(listRes) 
-
-    listRes2 = ''.join(listRes2)
-
-
-    listRes2 = re.findall(r'\d+[/]+\d+|\d+|\D', listRes2)
-
-    for x in listRes2:
-        if listRes2 == []:
-            break
-        else:
-            for i in token_start: 
-                if listRes2[0] == i:
-                    listRes2.pop(0)
-            for i in token_end:
-                if listRes2[-1] == i:
-                    listRes2.pop(-1)
-    listRes2 = ''.join(listRes2)
-
-    listRes = listRes.replace('++','+') \
-                     .replace('--','-') \
-                     .replace('+-','-') \
-                     .replace('-+','-') \
-                     .replace('&','^')
-
-    listRes2 = listRes2.replace('++','+') \
-                     .replace('--','-') \
-                     .replace('+-','-') \
-                     .replace('-+','-') \
-                     .replace('&','^')
-
-    clean_equation.append(listRes2) #сводим результат в финальный список
-    clean_equation.append(listRes)
-    return clean_equation   
-print(separator(string_input))
+            clean2.append(x)
+  
+    clean = ''.join(clean)
+    clean2 = ''.join(clean2)
+    clean_equation.append(clean2) 
+    clean_equation.append(clean)
+    return clean_equation
